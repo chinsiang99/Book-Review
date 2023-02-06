ae78fb2434a628ff7ecb3799ee019cfff6ee9ed5
chinsiang99 <chinsiang99@gmail.com>
2023-02-07 05:18:52 +0800

    feat(404-page): add 404 page error handling

--- middleware/error_handlerMiddleware.js
+++ middleware/error_handlerMiddleware.js
@@ -2,21 +2,22 @@
 const expressErrorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 404;
 
-  if(res.statusCode == 404 || res.statusCode == 401){
-    if(res.statusCode == 404){
+  if (res.statusCode == 404 || res.statusCode == 401) {
+    if (res.statusCode == 404) {
       err.message = "Page not Found!";
     }
     const data = {
       status: 401,
       errorMessage: err.message,
     }
-    res.render('login', data);
-  }else{
+    res.render('404Page', data);
+  } else {
     res.status(statusCode).json({
       status: statusCode,
       error: err.message
     })
   }
 };
+
 
 module.exports = { expressErrorHandler };
